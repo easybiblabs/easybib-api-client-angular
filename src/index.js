7a92ebb6380c3103cbaa45c4c61d1b6b0900689e
Max Stockner <mail@bluemaex.de>
2016-03-02 16:00:06 +0100

    move to a different library which supports safari private mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-require('angular-storage');
+require('angular-local-storage');
 
 module.exports = (function() {
   'use strict';
 
-  angular.module('easybib-api-client', [
-    'angular-storage'
-  ])
+  angular.module('easybib-api-client', ['LocalStorageModule'])
     .service('EasyBibApiClient', require('./easybib-api-client'));
 })();
